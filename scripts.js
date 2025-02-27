169aaf8f94a977b4ab6c2042eb8bbc45023551fb
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-02-27 01:55:37 -0500

    did problem 3, had some trouble figuring out, searched how to fix it withouth looking for answer right away

--- scripts.js
+++ scripts.js
@@ -7,9 +7,13 @@ function add7(number){
 console.log(add7(3));
 
 //another way to do it, shorter
+//arrow is used as a quick way to say return this expression result
 let add7Dif = (number) => number + 7;
 
 console.log(add7Dif(2));
+
+
+
 
 ///////////#2
 //one way to do it 
@@ -22,3 +26,22 @@ console.log(mult(2,4))
 //another way to do it, shorter
 let multDif = (num1, num2) => num1 * num2;
 console.log(multDif(5,5))
+
+
+
+////////#3
+//one way to do it 
+function capitalize(sentence){
+    //didn't know, but you can stack expressions
+    //gets the  first character(0), makes it upper case, slices the sentence at the second letter(1), makes that lower case
+    //then add it all back
+    let result = sentence.charAt(0).toUpperCase() + sentence.slice(1).toLowerCase()
+    return result
+}
+console.log(capitalize("sUsSY"))
+
+//another way to do it, shorter
+let capDif = (sentence) => sentence.charAt(0).toUpperCase() + sentence.slice(1).toLowerCase();
+console.log(capDif("heLLO i AM TrYing TO CODE right NoW."))
+
+
